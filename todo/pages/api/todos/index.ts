640575da9b8baee72e2f137f794ebdc3fe3b3c33
verycosy <verycosy@kakao.com>
2021-03-23 22:51:26 +0900

    add todo

--- todo/pages/api/todos/index.ts
+++ todo/pages/api/todos/index.ts
@@ -11,6 +11,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       res.statusCode = 500;
       res.send(err);
     }
+  } else if (req.method === "POST") {
+    const { text, color } = req.body;
+
+    if (!text || !color) {
+      res.statusCode = 400;
+      return res.send("text or color is empty");
+    }
+
+    const todos = Data.todo.getList();
+    let todoId: number = 1;
+
+    if (todos.length > 0) {
+      todoId = todos[todos.length - 1].id + 1;
+    }
+
+    const newTodo = {
+      id: todoId,
+      text,
+      color,
+      checked: false,
+    };
+
+    Data.todo.write([...todos, newTodo]);
+    res.statusCode = 200;
+    res.end();
   }
 
   res.statusCode = 405;
