bde722ad3354a15171a27f32bc19c9f0184b42e2
verycosy <verycosy@kakao.com>
2021-03-23 21:39:24 +0900

    useCallback, useMemo

--- todo/pages/index.tsx
+++ todo/pages/index.tsx
@@ -1,12 +1,17 @@
 import { NextPage } from "next";
-import styled from "styled-components";
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
 
-const Container = styled.div`
-  font-style: italic;
-`;
+const todos: TodoType[] = [
+  { id: 1, text: "장보기1", color: "red", checked: false },
+  { id: 2, text: "장보기2", color: "orange", checked: true },
+  { id: 3, text: "장보기3", color: "yellow", checked: false },
+  { id: 4, text: "장보기4", color: "green", checked: false },
+  { id: 5, text: "장보기5", color: "navy", checked: false },
+];
 
 const index: NextPage = () => {
-  return <Container>Hello, World!</Container>;
+  return <TodoList todos={todos} />;
 };
 
 export default index;
---
+++ todo/styles/palette.ts
@@ -0,0 +1,11 @@
+export default {
+  red: "red",
+  orange: "orange",
+  yellow: "yellow",
+  green: "green",
+  blue: "blue",
+  navy: "navy",
+  gray: "gray",
+  deep_red: "#F35456",
+  deep_green: "#47E774",
+};
