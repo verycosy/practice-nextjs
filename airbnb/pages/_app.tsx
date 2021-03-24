31c8bd1aba53683f6666ba9c355eb03fa71ede88
verycosy <verycosy@kakao.com>
2021-03-24 20:01:34 +0900

    header

--- airbnb/pages/_app.tsx
+++ airbnb/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
